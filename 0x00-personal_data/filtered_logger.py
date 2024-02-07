c21fa7e06e5c10a7ed469e2e28f3c95220dc0b4d
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-02-07 18:26:51 +0200

    Personal data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,12 +5,16 @@ Personal data
 """
 
 from re import sub
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
     """
     filter_datum
     """
     for field in fields:
         res = sub(f"{field}=[^{separator}]+", f"{field}={redaction}", message)
+        message = res
     return res
