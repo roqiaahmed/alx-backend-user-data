2debe627042a47d4261760f8f8362b449cc73382
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-02-13 08:40:29 +0200

    class Auth

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" Module of Auth class 
+"""
+
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    """Auth class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """require auth function"""
+        if path is None or excluded_paths is None:
+            return True
+        for p in excluded_paths:
+            if path[-1] != "/":
+                path += "/"
+            if p == path:
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """authorization header function"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar("User"):
+        """current user function"""
+        return None
