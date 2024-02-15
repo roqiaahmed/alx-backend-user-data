47c82ec1610973c90ad373a311d493a193f24e75
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-02-15 13:06:02 +0200

    5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -71,11 +71,11 @@ def do_something_before_request():
         and auth.session_cookie(request) is None
     ):
         abort(401)
-    if auth.authorization_header(request) is None:
-        abort(401)
     request.current_user = auth.current_user(request)
     if auth.current_user(request) is None:
         abort(403)
+    # if auth.authorization_header(request) is None:
+    #     abort(401)
 
 
 if __name__ == "__main__":
