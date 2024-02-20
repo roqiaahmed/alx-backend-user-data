2a73442e9de5f2c8d296e1f2cd6c55f86f0144f1
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-02-20 16:00:27 +0200

    7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,9 +2,12 @@
 """Flask app
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+from sqlalchemy.orm.exc import NoResultFound
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"])
@@ -12,5 +15,16 @@ def index() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route("/users", methods=["POST"])
+def users():
+    user_email = request.form.get("email")
+    user_password = request.form.get("password")
+    try:
+        AUTH.register_user(user_email, user_password)
+        return jsonify({"email": f"{user_email}", "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port="5001")
