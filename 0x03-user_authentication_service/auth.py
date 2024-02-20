4771ca7634efc430252308e964b1f0409d45c829
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-02-20 18:01:44 +0200

    10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -46,3 +46,12 @@ class Auth:
             return False
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """create session method"""
+        try:
+            user = self._db.find_user_by(email=email)
+            user.session_id = _generate_uuid()
+        except NoResultFound:
+            return None
+        return user.session_id
