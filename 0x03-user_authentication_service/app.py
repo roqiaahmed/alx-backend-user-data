f42271d67d9653b81eb1d5dbefea14d1f02f1121
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-02-22 09:37:10 +0200

    15

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -45,7 +45,7 @@ def logout():
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         AUTH.destroy_session(user.id)
-        return redirect(url_for(index))
+        return redirect(url_for("index"))
     abort(403)
 
 
